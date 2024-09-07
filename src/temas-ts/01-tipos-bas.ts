4858f5669e33465a0d68d8c0c09438d028e4edcf
Miguel Angel Rodriguez Ortiz <21000597@alumnos.utleon.edu.mx>
2024-09-07 10:04:56 -0600

    primer commit intro-ts

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,18 @@
+var num1=23;
+let num2='Juan';
+const num3=34; 
+
+const sum= num1 + num2 + num3;
+
+console.log(`La suma es: ${sum}`)
+
+let nombre:string='juan';
+let edad:number = 23;
+let activo:boolean = true;
+
+//edad='dario'
+
+let matricula:number|string;
+matricula=123456;
+matricula='se243de'
+
